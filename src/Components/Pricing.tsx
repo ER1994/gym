e41e5f01ae5c5b053af26bf4c593882dc245b173
ER1994 <94571379+ER1994@users.noreply.github.com>
2023-10-16 20:15:01 +0200

    modifying Get started button on each  and pricing 2

--- src/Components/Pricing.tsx
+++ src/Components/Pricing.tsx
@@ -98,7 +98,7 @@ export const Pricing = () => {
                                 <span>Free updates: <span className="font-semibold">36 months</span></span>
                             </li>
                         </ul>
-                        <a href="#" className="text-custom-blue bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
+                        <a href="#" className="text-black bg-primary-600 hover:bg-custom-blue focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
                     </div>
                 </div>
             </div>
