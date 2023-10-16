b313cf9bc55802c9d6a66a2e9cd8202e7a38b0a3
ER1994 <94571379+ER1994@users.noreply.github.com>
2023-10-16 20:04:55 +0200

    modifying Get started button on each  and pricing

--- src/Components/Pricing.tsx
+++ src/Components/Pricing.tsx
@@ -32,7 +32,7 @@ export const Pricing = () => {
                                 <span>Premium support: <span className="font-semibold">6 months</span></span>
                             </li>
                             <li className="flex items-center space-x-3">
-                                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                                <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="custom-blue" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                                 <span>Free updates: <span className="font-semibold">6 months</span></span>
                             </li>
                         </ul>
@@ -98,7 +98,7 @@ export const Pricing = () => {
                                 <span>Free updates: <span className="font-semibold">36 months</span></span>
                             </li>
                         </ul>
-                        <a href="#" className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
+                        <a href="#" className="text-custom-blue bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">Get started</a>
                     </div>
                 </div>
             </div>
